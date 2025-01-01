771c83a5865ace32d73733e1d821f22241bef952
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-01 16:55:36 +0100

    feat: rand and readnum functions

--- solution.js
+++ solution.js
@@ -1,9 +1,28 @@
-import { read, print, println, say } from "./lib.js"
+import { print, println, say, read, readnum, rand } from "./lib.js"
 
+// Hello world
 println("What's your name?")
-const name = read()
-print("Hello, ")
-println(name)
-
+let name
+name = read()
 say("Hello, ")
 say(name)
+
+
+
+// Guess a number
+println("How many stars I have?")
+
+let stars
+stars = rand(5)
+let guess
+guess = readnum()
+
+if (guess === stars) {
+  println("You're right!")
+}
+
+if (guess !== stars) {
+  print("No, I have ")
+  print(stars)
+  println(" stars")
+}
